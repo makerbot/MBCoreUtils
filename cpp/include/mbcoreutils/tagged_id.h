7997ddce6369182ec0e15ef750f37e5eac5b1470
Nicholas Bishop <nicholas.bishop@makerbot.com>
2013-08-27 13:35:00 -0400

    Change the TaggedID implementation into a macro
    This makes it easy to forward declare a tagged ID, and hence much
    easier to replace existing IDs without making lots of changes
    everywhere.

--- cpp/include/mbcoreutils/tagged_id.h
+++ cpp/include/mbcoreutils/tagged_id.h
@@ -7,136 +7,126 @@
 
 namespace MakerBot {
 
+/// Error thrown when attempting to use an invalid ID
+class IDError : public std::runtime_error {
+ public:
+  IDError()
+      : std::runtime_error("Invalid ID") {
+  }
+};
+
 /// Create a new ID type
-///
-/// The ID type will be a completely separate type from any other
-/// tagged ID.
-#define CREATE_TAGGED_ID_TYPE(name_, validator_, value_type_) \
-  class name_##_tag_; \
-  typedef MakerBot::TaggedID<value_type_, validator_, name_##_tag_> name_
-
-
-/// Identifier template
 ///
 /// This is a generic ID for ID types that can represent both valid
 /// and invalid values (in other words, at least one value of
 /// ValueType must be able to represent an invalid ID).
 ///
-/// The Validator type must provide two static methods: invalidValue()
-/// and valid(). The first returns a ValueType representing an invalid
-/// ID. The second takes a ValueType parameter and returns true if the
-/// parameter is valid, false otherwise.
+/// The new type will be named name_ and have a completely separate
+/// type from any other tagged ID. This is implemented as a macro
+/// rather than a templated class so that forward declarations are
+/// easy.
 ///
-/// The Tag type is used to make each TaggedID incompatible with all
-/// other TaggedIDs so that type errors are caught. The
-/// CREATE_TAGGED_ID_TYPE macro takes care of creating the Tag type.
+/// The id_policy_ type must provide three things:
+///
+/// - A ValueType typedef that will be used as the underlying storage
+///   for the ID.
+///
+/// - A static method invalidValue() that returns a ValueType
+///   representing an invalid ID.
+///
+/// - A static method valid() that a ValueType parameter and returns
+///   true if the parameter is valid, false otherwise.
+///
+/// The default constructor creates an invalid ID, the one-argument
+/// constructor produces a valid ID (an IDError is thrown if the
+/// argument is not a valid ID.)
+///
+/// The valid() method returns true if the current ID is valid, false
+/// otherwise.
+///
+/// The get() method returns the current ID if valid, otherwise an
+/// IDError is thrown.
+///
+/// Equality and less-than operators are provided. Invalid IDs are
+/// considered equal to eachother. An invalid ID is always less-than a
+/// valid ID.
 ///
 /// Example:
 ///
 ///   If a valid ID is a number between 0 and 127 (inclusive), a
-///   TaggedID could be created using a ValueType of signed
-///   char. Validator::invalidValue() could return -1 (or any negative
-///   number down to -128.)
+///   tagged ID could be created using a ValueType of signed
+///   char. ValidatorType::invalidValue() could return -1 (or any
+///   negative number down to -128.)
 ///
 ///   Code:
 ///
-///   struct MyIDValidator {
-///     static signed char invalidValue() { return -1; }
-///     static bool valid(const signed char value) { return value >= 0; }
+///   struct MyIDPolicy {
+///     typedef signed char ValueType;
+///     static ValueType invalidValue() { return -1; }
+///     static bool valid(const ValueType value) { return value >= 0; }
 ///   };
 ///
-///   CREATE_TAGGED_ID_TYPE(MyID, MyIDValidator, signed char);
-template<typename ValueType, typename Validator, typename Tag>
-class TaggedID {
- public:
-  /// Error thrown when attempting to use an invalid ID
-  class IDError : public std::runtime_error {
-   public:
-    IDError()
-        : std::runtime_error("Invalid ID") {
-    }
-  };
-
-  /// Construct an invalid ID
-  TaggedID()
-      : m_id(Validator::invalidValue()) {
+///   CREATE_TAGGED_ID_TYPE(MyID, MyIDPolicy);
+#define CREATE_TAGGED_ID_TYPE(name_, policy_) \
+  class name_ { \
+   public: \
+    typedef policy_ PolicyType; \
+    typedef PolicyType::ValueType ValueType; \
+    \
+    name_() \
+        : m_id(PolicyType::invalidValue()) { \
+    } \
+    \
+    explicit name_(const ValueType &id) \
+        : m_id(id) { \
+      if (!PolicyType::valid(id)) { \
+        throw MakerBot::IDError(); \
+      } \
+    } \
+    \
+    bool valid() const { \
+      return PolicyType::valid(m_id); \
+    } \
+    \
+    ValueType get() const { \
+      if (valid()) { \
+        return m_id; \
+      } else { \
+        throw MakerBot::IDError(); \
+      } \
+    } \
+    \
+    bool operator==(const name_ &other) { \
+      const bool meValid(valid()); \
+      const bool otherValid(other.valid()); \
+      \
+      if (meValid && otherValid) { \
+        return get() == other.get(); \
+      } else if (!meValid && !otherValid) { \
+        return true; \
+      } else { \
+        return false; \
+      } \
+    } \
+    \
+    bool operator<(const name_ &other) { \
+      const bool meValid(valid()); \
+      const bool otherValid(other.valid()); \
+      \
+      if (meValid && otherValid) { \
+        return get() < other.get(); \
+      } else if (!meValid && otherValid) { \
+        return true; \
+      } else if (meValid && !otherValid) { \
+        return false; \
+      } else { \
+        return false; \
+      } \
+    } \
+    \
+   private: \
+    ValueType m_id; \
   }
-
-  /// Construct a valid ID
-  ///
-  /// If an invalid ID is given, an IDError is thrown.
-  explicit TaggedID(const ValueType &id)
-      : m_id(id) {
-    if (!Validator::valid(id)) {
-      throw IDError();
-    }
-  }
-
-  /// Get the current ID
-  ///
-  /// If the current ID is invalid, an IDError is thrown.
-  ValueType get() const {
-    if (valid()) {
-      return m_id;
-    } else {
-      throw IDError();
-    }
-  }
-
-  /// Return true if the current ID is valid, false otherwise
-  bool valid() const {
-    return Validator::valid(m_id);
-  }
-
- private:
-  ValueType m_id;
-};
-
-/// TaggedID equality test
-///
-/// If both IDs are invalid they are considered equal. If one is valid
-/// and the other not, they are not equal. If both are valid their
-/// values are tested for equality.
-template<typename A, typename B, typename C,
-         typename D, typename E, typename F>
-bool operator==(
-    const TaggedID<A, B, C> &id1,
-    const TaggedID<D, E, F> &id2) {
-  const bool id1Valid(id1.valid());
-  const bool id2Valid(id2.valid());
-
-  if (id1Valid && id2Valid) {
-    return id1.get() == id2.get();
-  } else if (!id1Valid && !id2Valid) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-/// TaggedID comparison test
-///
-/// An invalid ID considered less than a valid ID. If both are invalid
-/// they are considered equal. If both are valid their values are
-/// tested for equality.
-template<typename A, typename B, typename C,
-         typename D, typename E, typename F>
-bool operator<(
-    const TaggedID<A, B, C> &id1,
-    const TaggedID<D, E, F> &id2) {
-  const bool id1Valid(id1.valid());
-  const bool id2Valid(id2.valid());
-
-  if (id1Valid && id2Valid) {
-    return id1.get() < id2.get();
-  } else if (!id1Valid && id2Valid) {
-    return true;
-  } else if (id1Valid && !id2Valid) {
-    return false;
-  } else {
-    return false;
-  }
-}
 }
 
 #endif  // CPP_INCLUDE_MBCOREUTILS_TAGGED_ID_H_
