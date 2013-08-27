2186670a65e673d97228c6b380b0c2bd89d251e4
Nicholas Bishop <nicholas.bishop@makerbot.com>
2013-08-27 12:58:52 -0400

    Add an ID templated class that can replace a lot of ad-hoc ID classes

---
+++ cpp/include/mbcoreutils/tagged_id.h
@@ -0,0 +1,96 @@
+// Copyright 2013 MakerBot Industries
+
+#ifndef CPP_INCLUDE_MBCOREUTILS_TAGGED_ID_H_
+#define CPP_INCLUDE_MBCOREUTILS_TAGGED_ID_H_
+
+#include <stdexcept>
+
+namespace MakerBot {
+
+/// Create a new ID type
+///
+/// The ID type will be a completely separate type from any other
+/// tagged ID.
+#define CREATE_TAGGED_ID_TYPE(name_, validator_, value_type_) \
+  class name_##_tag_; \
+  typedef MakerBot::TaggedID<value_type_, validator_, name_##_tag_> name_
+
+
+/// Identifier template
+///
+/// This is a generic ID for ID types that can represent both valid
+/// and invalid values (in other words, at least one value of
+/// ValueType must be able to represent an invalid ID).
+///
+/// The Validator type must provide two static methods: invalidValue()
+/// and valid(). The first returns a ValueType representing an invalid
+/// ID. The second takes a ValueType parameter and returns true if the
+/// parameter is valid, false otherwise.
+///
+/// The Tag type is used to make each TaggedID incompatible with all
+/// other TaggedIDs so that type errors are caught. The
+/// CREATE_TAGGED_ID_TYPE macro takes care of creating the Tag type.
+///
+/// Example:
+///
+///   If a valid ID is a number between 0 and 127 (inclusive), a
+///   TaggedID could be created using a ValueType of signed
+///   char. Validator::invalidValue() could return -1 (or any negative
+///   number down to -128.)
+///
+///   Code:
+///
+///   struct MyIDValidator {
+///     static signed char invalidValue() { return -1; }
+///     static bool valid(const signed char value) { return value >= 0; }
+///   };
+///
+///   CREATE_TAGGED_ID_TYPE(MyID, MyIDValidator, signed char);
+template<typename ValueType, typename Validator, typename Tag>
+class TaggedID {
+ public:
+  /// Error thrown when attempting to use an invalid ID
+  class IDError : public std::runtime_error {
+   public:
+    IDError()
+        : std::runtime_error("Invalid ID") {
+    }
+  };
+
+  /// Construct an invalid ID
+  TaggedID()
+      : m_id(Validator::invalidValue()) {
+  }
+
+  /// Construct a valid ID
+  ///
+  /// If an invalid ID is given, an IDError is thrown.
+  explicit TaggedID(const ValueType &id)
+      : m_id(id) {
+    if (!Validator::valid(id)) {
+      throw IDError();
+    }
+  }
+
+  /// Get the current ID
+  ///
+  /// If the current ID is invalid, an IDError is thrown.
+  ValueType get() const {
+    if (valid()) {
+      return m_id;
+    } else {
+      throw IDError();
+    }
+  }
+
+  /// Return true if the current ID is valid, false otherwise
+  bool valid() const {
+    return Validator::valid(m_id);
+  }
+
+ private:
+  ValueType m_id;
+};
+}
+
+#endif  // CPP_INCLUDE_MBCOREUTILS_TAGGED_ID_H_
