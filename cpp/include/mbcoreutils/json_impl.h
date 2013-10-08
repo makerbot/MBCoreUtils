e97c46c6a66cc19e6144bb3098929a49afb72558
Nicholas Bishop <nicholasbishop@gmail.com>
2013-10-08 12:16:27 -0400

    Add new overload of Json::objectMemberAs

--- cpp/include/mbcoreutils/json_impl.h
+++ cpp/include/mbcoreutils/json_impl.h
@@ -3,6 +3,9 @@
 #ifndef CPP_INCLUDE_MBCOREUTILS_JSON_IMPL_H_
 #define CPP_INCLUDE_MBCOREUTILS_JSON_IMPL_H_
 
+#include <limits>
+#include <string>
+
 namespace Json {
 inline Json::Value parse(const std::string &text) {
   Json::Reader reader;
@@ -46,6 +49,23 @@ inline unsigned int objectMemberAs<unsigned int>(
   const auto member(objectMember(json, key));
   if (member.isNumeric()) {
     return member.asUInt();
+  } else {
+    throw TypeError(json.toStyledString());
+  }
+}
+
+template<>
+inline uint16_t objectMemberAs<uint16_t>(
+    const Json::Value &json,
+    const std::string &key) {
+  const auto member(objectMember(json, key));
+  if (member.isNumeric()) {
+    const auto v(member.asUInt());
+    if (v <= std::numeric_limits<uint16_t>::max()) {
+      return static_cast<uint16_t>(v);
+    } else {
+      throw std::out_of_range("objectMemberAs<uint16_t>: range error");
+    }
   } else {
     throw TypeError(json.toStyledString());
   }
