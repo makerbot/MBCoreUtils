e5d5532e613999c116d5940ef917d6504174f942
Seth Foster <seth.foster@makerbot.com>
2013-11-25 11:14:52 -0500

    Fixed an issue where files with multiple enums wouldn't be generated correctly

--- birdwing/enum_parse.py
+++ birdwing/enum_parse.py
@@ -32,32 +32,16 @@ def make_google_compliant(string):
     return string
 
 def parse_enum_c(filepath, filename, enum_data, namespace):
+    for group in enum_data:
+        print(group)
     cfilepath = "%s.hh" % (filepath)
-    stringifypath = "%s_stringify.cc" % (filepath)
+    #stringifypath = "%s_stringify.cc" % (filepath)
     namespace_top = "namespace " + namespace + " {\n"
-    stringify_error_top = "std::string StringifyError(Error error){\n"
-    stringify_error_bottom = "}\n"
-    namespace_bottom = "}"
-    c_define_top = "#ifndef %s_HH\n#define %s_HH\n#ifndef BRONX\n#include <string>\n#endif\n" % (filename.upper(), filename.upper())
+    namespace_bottom = "} //namespace"
+    c_define_top = "#ifndef %s_HH\n#define %s_HH\n#ifndef BRONX\n#include <string>\n#include <sstream>\n#endif\n" % (filename.upper(), filename.upper())
     c_define_bottom = "#endif // %s_HH" % (filename.upper())
-    stringify_file_top = "#ifndef BRONX\n#include <string>\n#include <sstream>\n#include \"" + filename + ".hh\" \n"
-    stringify_file_bottom = "#endif"
-    with open(stringifypath, 'w') as f:
-        f.write(stringify_file_top)
-        for group in enum_data:
-            c_codes = {}
-            for key in enum_data[group]:
-                c_codes["k%s" % (key)] = enum_data[group][key]
-            stringify_top = "std::string %s::stringify_%s(%s val) {\n\tstd::stringstream retval;\n\tswitch(val){\n" \
-                 % (make_google_compliant(group), make_google_compliant(group), namespace)
-            stringify_middle = ""
-            for key, value in enum_data[group].iteritems():
-                stringify_middle += "\tcase %i:\n\t\tretval << \"%s::k%s\";\n\t\tbreak;\n" \
-                     % (value,namespace,str(make_google_compliant(key)))
-            stringify_bottom = "\tdefault:\n\t\tretval << \"%s::Unknown or not implemented\";\n\t}\n\treturn retval.str();\n}" % (namespace)
-            for part in [ stringify_top, stringify_middle, stringify_bottom]:
-                f.write("%s\n" % (part))
-        f.write(stringify_file_bottom)
+    stringify_file_top = "#ifndef BRONX\n"
+    stringify_file_bottom = "#endif // BRONX\n"
 
     with open(cfilepath, 'w') as f:
         for part in [c_define_top, namespace_top]:
@@ -67,15 +51,27 @@ def parse_enum_c(filepath, filename, enum_data, namespace):
             c_codes = {}
             for key in enum_data[group]:
                 c_codes["k%s" % (key)] = enum_data[group][key]
-            stringify_prototype = "#ifndef BRONX\nstd::string stringify_%s(%s val);\n#endif\n" % (make_google_compliant(group), make_google_compliant(group))
             declaration_top = "enum %s {" % (make_google_compliant(group))
             declaration_middle = ""
             for key, value in enum_data[group].iteritems():
                 declaration_middle += "\tk%s = %i,\n" % (make_google_compliant(key), value)
-            declaration_bottom = "};\n"
-            for part in [ declaration_top, declaration_middle, declaration_bottom,stringify_prototype]:
+            declaration_bottom = "}; //enum \n"
+            for part in [declaration_top, declaration_middle, declaration_bottom]:
+                f.write("%s\n" % part)
+            f.write(stringify_file_top)
+            c_codes = {}
+            for key in enum_data[group]:
+                c_codes["k%s" % (key)] = enum_data[group][key]
+            stringify_top = "inline std::string stringify_%s(%s val) {\n\tstd::stringstream retval;\n\tswitch(val){\n" \
+                 % (make_google_compliant(group).lower(), make_google_compliant(group))
+            stringify_middle = ""
+            for key, value in enum_data[group].iteritems():
+                stringify_middle += "\tcase %i:\n\t\tretval << \"%s::k%s\";\n\t\tbreak;\n" \
+                     % (value,namespace,str(make_google_compliant(key)))
+            stringify_bottom = "\tdefault:\n\t\tretval << \"%s::Unknown or not implemented\";\n\t}\n\treturn retval.str();\n} //stringify" % (namespace)
+            for part in [ stringify_top, stringify_middle, stringify_bottom]:
                 f.write("%s\n" % (part))
-
-        for part in [ namespace_bottom, c_define_bottom]:
+            f.write(stringify_file_bottom)
+        for part in [namespace_bottom, c_define_bottom]:
             f.write("%s\n" % (part))
 
