a7c04d06fd8b8f598d03f5aaf5916fa447abde82
Nicholas Bishop <nicholasbishop@gmail.com>
2014-01-17 18:32:34 -0500

    Add MakerBot::Meters class for length storage

---
+++ include/mbcoreutils/meters.h
@@ -0,0 +1,64 @@
+// Copyright 2014 MakerBot Industries
+
+#ifndef INCLUDE_MBCOREUTILS_METERS_H_
+#define INCLUDE_MBCOREUTILS_METERS_H_
+
+#include <stdexcept>
+
+namespace MakerBot {
+
+/// Stores a measurement of length vaguely in the style of std::chrono
+///
+/// The internal storage is a double.
+///
+/// The purpose of this class is to avoid dealing in raw numbers
+/// without corresponding units.
+class Meters {
+ public:
+  typedef double ValueType;
+
+  /// Initialize with zero length
+  Meters()
+      : m_meters(0.0) {
+  }
+
+  /// Create a new Meters object by converting from millimeters
+  static Meters meters(const ValueType meters) {
+    return Meters(meters);
+  }
+
+  /// Create a new Meters object by converting from millimeters
+  static Meters millimeters(const ValueType millimeters) {
+    return Meters(millimeters * 0.001);
+  }
+
+  /// Get the length in meters
+  ValueType inMeters() const {
+    return m_meters;
+  }
+
+  /// Convert the length to millimeters
+  ValueType inMillimeters() const {
+    return m_meters * 1000.0;
+  }
+
+  Meters &operator=(const Meters &other) {
+    m_meters = other.m_meters;
+    return *this;
+  }
+
+ private:
+  /// Initialize with the given meters value
+  explicit Meters(const ValueType meters)
+      : m_meters(meters) {
+    if (m_meters < 0.0) {
+      throw std::out_of_range("Meters value cannot be negative");
+    }
+  }
+
+  /// Current value
+  double m_meters;
+};
+}
+
+#endif  // INCLUDE_MBCOREUTILS_METERS_H_
