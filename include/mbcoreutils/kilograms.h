92175a618646c85dc51c4907bca33615b3b70adb
Nicholas Bishop <nicholasbishop@gmail.com>
2014-01-17 18:32:52 -0500

    Rewrite MakerBot::Weight as MakerBot::Kilograms, similar to Meters

---
+++ include/mbcoreutils/kilograms.h
@@ -0,0 +1,64 @@
+// Copyright 2014 MakerBot Industries
+
+#ifndef INCLUDE_MBCOREUTILS_WEIGHT_H_
+#define INCLUDE_MBCOREUTILS_WEIGHT_H_
+
+#include <stdexcept>
+
+namespace MakerBot {
+
+/// Stores a measurement of weight vaguely in the style of std::chrono
+///
+/// Kilograms is used as the base unit because that's what SI does.
+///
+/// The internal storage is a double.
+///
+/// The purpose of this class is to avoid dealing in raw numbers
+/// without corresponding units.
+class Kilograms {
+ public:
+  typedef double ValueType;
+
+  /// Initialize with zero weight
+  Kilograms()
+      : m_kilograms(0.0) {
+  }
+
+  static Kilograms kilograms(const ValueType kilograms) {
+    return Kilograms(kilograms);
+  }
+
+  static Kilograms pounds(const ValueType pounds) {
+    return Kilograms(pounds * 0.453592);
+  }
+
+  /// Get the weight in kilograms
+  ValueType inKilograms() const {
+    return m_kilograms;
+  }
+
+  /// Convert the weight to pounds
+  ValueType inPounds() const {
+    return m_kilograms * 2.20462;
+  }
+
+  Kilograms &operator=(const Kilograms &other) {
+    m_kilograms = other.m_kilograms;
+    return *this;
+  }
+
+ private:
+  /// Initialize with the given kilograms value
+  explicit Kilograms(const ValueType kilograms)
+      : m_kilograms(kilograms) {
+    if (m_kilograms < 0.0) {
+      throw std::out_of_range("Kilograms value cannot be negative");
+    }
+  }
+
+  /// Current value
+  double m_kilograms;
+};
+}
+
+#endif  // INCLUDE_MBCOREUTILS_WEIGHT_H_
