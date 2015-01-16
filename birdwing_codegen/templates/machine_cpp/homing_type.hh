9c80f1337df0bb7301b0bd44bc30fd44b5c245f9
Bill Canfield <wdc.rith@gmail.com>
2015-01-16 17:48:55 -0500

    BW-2150: Adding HomingType<->std::string methods, as well as the number of homing types, for machine_cpp

--- birdwing_codegen/templates/machine_cpp/homing_type.hh
+++ birdwing_codegen/templates/machine_cpp/homing_type.hh
@@ -2,9 +2,17 @@
 // Intended for use in the machine-driver.
 // Copyright 2015 MakerBot
 
-
 #ifndef BIRDWING_CODEGEN_TEMPLATES_MACHINE_CPP_HOMING_TYPE_HH_
 #define BIRDWING_CODEGEN_TEMPLATES_MACHINE_CPP_HOMING_TYPE_HH_
+
+#include <string>
+
+
+namespace machine {
+
+// Find a nice way of calculating number of valid homing types via pystache
+
+static const int NUM_HOMING_TYPES = {{number_homing_types}};
 
     enum HomingType {
         {{#homing_types}}
@@ -12,4 +20,31 @@
         {{/homing_types}}
     };
 
-#endif // BIRDWING_CODEGEN_TEMPLATES_MACHINE_CPP_HOMING_TYPE_HH_
+inline std::string homing_type_to_string(HomingType homing_type) {
+    switch (homing_type) {
+    {{#homing_types}}
+    case {{name}}:
+        return "{{name_str}}";
+
+    {{/homing_types}}
+    default:
+        return "";
+    }
+}
+
+inline HomingType string_to_homing_type(std::string string) {
+    {{#homing_types}}
+    if (string.compare("{{name_str}}") == 0) {
+        return {{name}};
+    }
+    {{/homing_types}}
+    else {
+        return HomingType::kInvalid;
+    }
+}
+
+
+} // end machine namespace
+
+
+#endif  // BIRDWING_CODEGEN_TEMPLATES_MACHINE_CPP_HOMING_TYPE_HH_
