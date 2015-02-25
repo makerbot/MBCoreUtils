550d6d1bea74be256d60cc06619ea47add145a81
Seth Foster <seth.foster@makerbot.com>
2015-02-25 12:18:07 -0500

    get extruder type name in tool mappings

--- birdwing_codegen/templates/python/tool_mappings.py
+++ birdwing_codegen/templates/python/tool_mappings.py
@@ -3,7 +3,8 @@
 tool_to_string = {
     {{#tools}}
         0x{{id}}: {'pretty_name' : u'{{pretty_name}}',
-                   'short_name' : u'{{name}}'},
+                   'short_name' : u'{{name}}',
+                   'type_name': u'{{type}}'},
     {{/tools}}
 }
 
