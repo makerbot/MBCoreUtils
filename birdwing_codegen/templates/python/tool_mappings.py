55dc202497c2931d123cb1ca72f3c605da2bfc1b
Seth Foster <seth.foster@makerbot.com>
2015-03-05 16:34:31 -0500

    Add material and type generation for libtinything
    This is necessary to handle the new .makerbot stuff, where much more
    logic in parsing the metafile is handled in libtinything.
    BW-1925
    BW-1901
    http://jira.makerbot.net/browse/BW-1925

--- birdwing_codegen/templates/python/tool_mappings.py
+++ birdwing_codegen/templates/python/tool_mappings.py
@@ -4,7 +4,7 @@ tool_to_string = {
     {{#tools}}
         0x{{id}}: {'pretty_name' : u'{{pretty_name}}',
                    'short_name' : u'{{name}}',
-                   'type_name': u'{{type}}'},
+                   'type_name': u'{{type_name}}'},
     {{/tools}}
 }
 
@@ -12,4 +12,10 @@ name_to_id = {
     {{#tools}}
         "{{name}}": {{id}},
     {{/tools}}
+}
+
+id_to_material = {
+    {{#materials}}
+        {{id}} : "{{name}}",
+    {{/materials}}
 }
--- birdwing_codegen/templates/shared_cpp/tool_mappings.hh
+++ birdwing_codegen/templates/shared_cpp/tool_mappings.hh
@@ -14,10 +14,22 @@ namespace bwcoreutils {
 class YonkersTool {
 public:
 
-    enum TOOL {
+    enum class TOOL : int {
         {{#tools}}
-        {{name}} = 0x{{id}},
+        {{name}} = {{id}},
         {{/tools}}
+    };
+
+    enum class TYPE : int {
+        {{#types}}
+        {{name}},
+        {{/types}}
+    };
+
+    enum class MATERIALS : int {
+        {{#materials}}
+        {{name}} = {{id}},
+        {{/materials}}
     };
 
     YonkersTool(uint8_t id) :
@@ -27,7 +39,7 @@ public:
     std::string name() const {
         switch (m_tool) {
             {{#tools}}
-            case {{name}}:
+            case TOOL::{{name}}:
                 return "{{pretty_name}}";
             {{/tools}}
             default:
@@ -35,9 +47,52 @@ public:
         }
     }
 
+    TYPE type() const {
+        switch (m_tool) {
+            {{#tools}}
+        case TOOL::{{name}}:
+            return TYPE::{{type_name}};
+        {{/tools}}
+        default:
+            return TYPE::UNKNOWN;
+        }
+    }
+
+    static TYPE type_from_type_name(const std::string& type_name) {
+        // This weird formatting brought to you by switch() only
+        // knowing how to handle integral types for some reason
+        if(type_name == "") {
+            return TYPE::UNKNOWN;
+        }
+        {{#types}}
+        else if(type_name == std::string("{{key}}")) {
+            return TYPE::{{name}}; }
+        {{/types}}
+        else {
+            return TYPE::UNKNOWN;
+        }
+    }
+
+    static std::string material_from_material_type(const MATERIALS mat) {
+        switch(mat) {
+            {{#materials}}
+        case MATERIALS::{{name}}:
+            return std::string("{{name}}");
+            {{/materials}}
+        default:
+            return std::string("Unknown");
+        }
+    }
+
     TOOL tool() const { return m_tool; }
 
-    bool operator==(const YonkersTool& rValue){ return this->m_tool == rValue.m_tool; }
+    bool operator==(const YonkersTool& rValue) const {
+        return this->m_tool == rValue.m_tool;
+    }
+
+    bool type_match(const YonkersTool& rValue) const {
+        return type() == rValue.type();
+    }
 
 private:
     TOOL m_tool;
