c0dfd2064e4638b4c2b6d943553e2f20ccc9a906
Jackson Howard <jackson.howard@makerbot.com>
2015-03-09 12:36:06 -0400

    implement more generic kaiten api interface
    with the bonus of callbacks actually working this time.
    
    We now codegen a file called 'kaitenstub.hh' which contains a class
    'KaitenStub' that provides an interface for the kaiten api.  A 'KaitenStub'
    object can be constructed w/ any object implementing the krpc::Controller
    interface (see ikrpccontroller.hh).  The 'KaitenStub' object can then be used
    to make strongly-typed api calls - under the hood it uses the krpc::Controller
    object's generic invoke() method to make the actual rpc calls.
    
    For each api endpoint, there will be one or two functions that can be used to
    call it (depending on whether the endpoint takes optional params).  As an
    example, the codegen'd functions to invoke a print look something like this:
    KaitenStub::print(const std::string filepath,
                      boost::optional<const bool> ensure_build_plate_clear =
                            boost::none,
                      const std::shared_ptr<JsonRpcCallback>& callback =
                            std::make_shared<noCB>());
    
    KaitenStub::print(const std::string filepath,
                      const std::shared_ptr<JsonRpcCallback& callback);
    
    where the raw api endpoint metadata looks like:
    {
        "name": "print",
        "parameters": [
            {
                "doc": "Filepath to print",
                "name": "filepath",
                "type": "str"
            },
            {
                "default": null,
                "doc": "optional bool that overrides any other logic determining whether to show a clear build plate dialog prior to starting a print.",
                "name": "ensure_build_plate_clear",
                "type": "bool"
            }
        ]
    }
    
    With the above interface a print could be invoked any of the following ways:
        [stub instance]->print("test.makerbot", callback);
    or  [stub instance]->print("test.makerbot", true, callback);
    or  [stub instance]->print("test.makerbot", true);
    or  [stub instance]->print("test.makerbot");
    
    BW-2327 / related to BW-1798 & BW-2328

--- birdwing_codegen/contexts/kaiten_api.py
+++ birdwing_codegen/contexts/kaiten_api.py
@@ -5,7 +5,7 @@ import subprocess
 api_dict = {'kaiten_api': []}
 
 
-def add_param_metadata(api):
+def add_metadata(api):
     for fidx, func in enumerate(api):
         # both api func params & api funcs have a 'name' key, making it
         # impossible to reference the parent api func while parsing its
@@ -13,13 +13,20 @@ def add_param_metadata(api):
         # use to insert the parent rpc func name while iterating the params.
         api[fidx]['fn_name'] = func['name']
         for pidx, param in enumerate(func['parameters']):
-            # params are marked as being optional by the presence of a
-            # 'default' key. the 'default' key is often set to null, indicating
-            # the param is optional. this causes issues, as mustache evaluates
-            # both the absense of keys and keys set to null as False.  A null
-            # key vs an absent key has different semantics, so create a bool
-            # for params that explicitly indicates whether its optional.
-            api[fidx]['parameters'][pidx]['optional'] = 'default' in param
+            if 'default' in param:
+                # params are marked as being optional by the presence of a
+                # 'default' key. the 'default' key is often set to null,
+                # indicating the param is optional. this causes issues, as
+                # mustache evaluates both the absense of keys and keys set to
+                # null as False.  A null key vs an absent key has different
+                # semantics, so create a bool for params that explicitly
+                # indicates whether its optional.
+                api[fidx]['parameters'][pidx]['optional'] = True
+                # below flag is used to test if func has optional params w/out
+                # iterating through params. if true, an extra func can be
+                # generated in templates that only takes required params and a
+                # callback (for convenience - opt params are often unused):
+                api[fidx]['optional_params'] = True
     return api
 
 
@@ -30,7 +37,7 @@ def generate_context(env, target, source):
     if os.path.exists(apigen_path):
         api = json.loads(subprocess.check_output(
                          ['python3', apigen_path, '-l 3']))
-        api_dict['kaiten_api'] = add_param_metadata(api)
+        api_dict['kaiten_api'] = add_metadata(api)
     elif 'MBCOREUTILS_BIRDWING' in env:
         raise Exception('Unable to locate %s, required for birdwing builds!'
                         % apigen_path)
--- birdwing_codegen/templates/README.md
+++ birdwing_codegen/templates/README.md
@@ -6,7 +6,5 @@ Templates in each folder are used in different parts of the stack:
   -  These templates are written in CPP and their output is primarily used by the machine-driver and other low-level parts of the firmware.  The majority (all?) of machine-specific context generation is for these templates.
 - "[shared_cpp](shared_cpp)"
   - These templates are written in CPP and their output is primarily used by UI clients.  The context data used to render these templates should hopefully be machine-agnostic.
-- "[fwclient_cpp](fwclient_cpp)"
-  - These are templates whose output is used by firmware clients (queenie/printerpanel).  Eventually these may be moved into shared_cpp if their dependencies on firmware only code can be removed.  Uses machine-agnostic context data.
 - "[python](python)"
   - Primarily used by Kaiten, these templates are written in python and expose python-equivalent mappings and datastructures from what is implemented in machine_cpp and shared_cpp.
