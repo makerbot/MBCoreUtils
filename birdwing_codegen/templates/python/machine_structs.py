351e51065932f922dc42be2aa58bd86e9eda9cb9
Bill Canfield <wdc.rith@gmail.com>
2015-05-05 15:48:25 -0400

    BW-2530: Adding get_dict function to machine_structs.py classes, this is done so that kaiten doesn't have to generate dicts from ctypes.structs dynamically

--- birdwing_codegen/templates/python/machine_structs.py
+++ birdwing_codegen/templates/python/machine_structs.py
@@ -4,12 +4,24 @@ import ctypes
 
 {{#structs}}
 
-class {{name}}(ctypes.LittleEndianStructure):
-	_fields_ = {{#inherits}}{{inherits}}._fields_ + {{/inherits}}(
-		{{#fields}}
-		("{{name}}", {{type}}),
-		{{/fields}}
-	)
+class {{name}}({{#inherits}}{{inherits}}, {{/inherits}}ctypes.LittleEndianStructure):
+    _fields_ = {{#inherits}}{{inherits}}._fields_ + {{/inherits}}(
+        {{#fields}}
+        ("{{name}}", {{type}}),
+        {{/fields}}
+    )
+
+    def get_dict(self):
+        field_dict = {}
+        {{#fields}}
+        if hasattr(self.{{name}}, "get_dict"):
+            field_dict["{{name}}"] = self.{{name}}.get_dict()
+        else:
+            field_dict["{{name}}"] = self.{{name}}
+
+        {{/fields}}
+        {{#inherits}}field_dict.update(super({{name}}, self).get_dict()){{/inherits}}
+        return field_dict
 {{/structs}}
 
 {{#utility}}
@@ -19,4 +31,16 @@ class {{name}}(ctypes.LittleEndianStructure):
         ("{{name}}", {{type}}),
         {{/fields}}
     )
+
+    def get_dict(self):
+        field_dict = {}
+        {{#fields}}
+        if hasattr(self.{{name}}, "get_dict"):
+            field_dict["{{name}}"] = self.{{name}}.get_dict()
+        else:
+            field_dict["{{name}}"] = self.{{name}}
+
+        {{/fields}}
+        return field_dict
+
 {{/utility}}
