003dbb3b7e146283a7e02cc8b3fcd593de2d1f45
Carlos Chinchilla <cachinchilla3000@yahoo.com.mx>
2015-07-23 16:58:55 -0400

    autogenerating options for when to autounload

---
+++ birdwing_codegen/templates/python/common_settings.py
@@ -0,0 +1,8 @@
+""" This file was automatically generated, do not edit it! """
+
+auto_unload_cases = [
+    {{#auto_unload_cases}}
+        u'{{name}}',
+    {{/auto_unload_cases}}
+]
+
---
+++ birdwing_codegen/templates/shared_cpp/common_settings.hh
@@ -0,0 +1,16 @@
+// This file was automatically generated, do not edit it!
+
+#ifndef COMMON_SETTINGS_HH
+#define COMMON_SETTINGS_HH
+#include <string>
+#include <vector>
+
+namespace PrinterPanel {
+  const std::array<std::string, 3> AUTO_UNLOAD_CASES =  {
+  {{#auto_unload_cases}}
+  "{{name}}",
+  {{/auto_unload_cases}}
+  };
+} // namespace
+
+#endif // COMMON_SETTINGS_HH
