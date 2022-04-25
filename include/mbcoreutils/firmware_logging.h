e5e94a7f175589e974487c249e4e73ba6b078b5d
Chris Moore <chris.moore@makerbot.com>
2022-04-25 14:23:42 -0400

    Move boost logging setup to a common location
    BW-5632
    http://makerbot.atlassian.net/browse/BW-5632
    
    This takes the boost logging setup that we copied across almost a dozen
    repositories and places a single version here.  The header and source
    files are now both in the header, with all functions marked as inline
    to prevent ODR errors.  Static variables are also transitioned to local
    static variables, which is supposedly necessary to ensure that we don't
    create separate copies of these variables for each translation unit, but
    I did run some tests and it does _seem_ like static variables are not
    getting duplicated across translation units.
    
    One nuisance about moving all logging to a common location is that we
    were very much relying on each repository that set up logging to use a
    completely unique set of names for every variable/function/class defined
    for the general log in order to actually create distinct log sources
    with distinct channel names.  We could try to use the channel name to
    generate distinct names via macros but then we wouldn't be able to use
    libstepper/pru as a channel name anymore.  The only thing we are
    currently using distinct log sources within the general logs is to put
    the channel name in the log message, so we just use the logging macro
    to put this name into the log message instead.  Since this is now fully
    unrelated to the actual log channels (which we still use), we rename
    this to be a "context" instead of a "channel"
    
    The __FILENAME__ macro from the old logging setup has been replaced by
    passing the __FILE__ macro through a constexpr function, which has the
    advantage of getting the filename right even if we log from a header
    file (which we now do here in fact) and not needing build system support
    (though before this solution I did have the build system support down to
    a single utility function that would automatically add the macro to
    every single source file for a given target).  The disadvantages are
    that I couldn't find any C++11 way to force compile time evaluation of
    the function without breaking the macro (I manually checked that the
    evaluation does not happen at runtime but the compiler would be allowed
    to evaluate at runtime and still be compliant) and that the full file
    path is still stored in rodata (the compiler can't verify that we don't
    try to read backwards from the start of the substring).

---
+++ include/mbcoreutils/firmware_logging.h
@@ -0,0 +1,198 @@
+// Copyright MakerBot, Inc. 2022
+
+// This file provides wrappers around boost logging that provide the format
+// and structure of logs that we use for all firmware logging from C++
+// libraries.
+#ifndef INCLUDE_MBCOREUTILS_FIRMWARE_LOGGING_H_
+#define INCLUDE_MBCOREUTILS_FIRMWARE_LOGGING_H_
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-local-typedefs"
+#pragma GCC diagnostic ignored "-Wunused-variable"
+#include <boost/log/core.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/log/sources/global_logger_storage.hpp>
+#include <boost/log/sources/severity_channel_logger.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup.hpp>
+
+#include <locale>
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+#pragma GCC diagnostic pop
+
+// We require a logging context string to be defined in order to use logging.
+// The context string is supposed to identify the firmware component generating
+// the log message, so it should either be a single short static string or a
+// short static string with an index appended (via <<) when we have two or more
+// components sharing the same code.
+#ifndef LOGGING_CONTEXT
+#error Logging context must be defined to use logging
+#endif
+
+// The main API entry point for standard logging
+#define LOG(level) \
+    BOOST_LOG_SEV(Logging::general::get(), boost::log::trivial::level) << "["\
+    << LOGGING_CONTEXT << "] [" << Logging::getFilename(__FILE__) << ":"\
+    << __LINE__ << ":" << __func__ << "] "
+
+// The main API entry point for telemetry logging
+#define TELEM(level) \
+    BOOST_LOG_SEV(Logging::telemetry::get(), boost::log::trivial::level)
+
+namespace Logging {
+    // Initialize the logging backends.  No logs will be written to disk before
+    // this happens so this should be called as early as possible.  The folder
+    // to write all logs into, the base name for each log, and the size cap for
+    // each log are specified here.  The general log level will be set to info
+    // and the telemetry log will be off -- these should be changed as soon as
+    // possible to the configured level via the Change*Level functions, but we
+    // want to make sure that we log any errors that happen before we load these
+    // configurations.
+    //
+    // TODO(chris): The size caps for each individual log are not being honored,
+    //   instead the smallest value passed will be used a a size cap for both
+    //   logs combined.
+    inline void Initialize(const std::string& folder_name,
+        int general_size_mb, const std::string& general_file_name,
+        int telemetry_size_mb, const std::string& telemetry_file_name);
+
+    // Set the log level for each of the log backends.  We accept the strings
+    // "debug", "info", "warning", "error", "off" (case insensitive).
+    inline void ChangeGeneralLevel(const std::string& sev);
+    inline void ChangeTelemetryLevel(const std::string& sev);
+
+    // Implemenation
+    //
+    // The general configuration of boost logging is that you can make as many
+    // sources and as many sinks as you want, and through some boost singleton
+    // magic these will automatically all be connected up so that every source
+    // writes to every sink.  We want a general log that writes to one log file
+    // and a telemetry log that writes to a telemetry file, so we create two
+    // log sources with different channel names, and then two log sinks that
+    // always filter only the channel names that we want to log.
+
+    // C++11 constexpr function to reduce a full file path to a filename.
+    constexpr size_t getAfterSlashIdx(
+            const char * file, size_t idx, size_t res) {
+        return file[idx] ? getAfterSlashIdx(file, idx + 1,
+            file[idx] == '/' ? idx + 1 : res) : res;
+    }
+    constexpr const char * getFilename(const char * file) {
+        return file + getAfterSlashIdx(file, 0, 0);
+    }
+
+    // Typedef for logging sources
+    typedef boost::log::sources::severity_channel_logger
+        <boost::log::trivial::severity_level, std::string> log_class;
+
+    // Typedef for logging sinks
+    typedef boost::shared_ptr<boost::log::sinks::synchronous_sink  // NOLINT
+        <boost::log::sinks::text_file_backend > > backend_ptr;
+
+    // Use local statics to ensure that only one instance of each pointer is
+    // ever created by the linker.
+    inline backend_ptr & general_backend() {
+        static backend_ptr backend;
+        return backend;
+    }
+    inline backend_ptr & telemetry_backend() {
+        static backend_ptr backend;
+        return backend;
+    }
+
+    // Create boost singletons for the logging sources -- these declare the
+    // identifiers general and telemetry which have a ::get() static method
+    // to retrieve a singleton of type log_class.
+    BOOST_LOG_INLINE_GLOBAL_LOGGER_INIT(general, log_class) {
+        log_class lg(boost::log::keywords::channel = "general");
+        return lg;
+    }
+    BOOST_LOG_INLINE_GLOBAL_LOGGER_INIT(telemetry, log_class) {
+        log_class lg(boost::log::keywords::channel = "telemetry");
+        return lg;
+    }
+
+    // Helper function for changing the log level -- we always need the filter
+    // to both select the log level and only allow entries from one channel.
+    template <int Level>
+    void SetLevel(backend_ptr & backend, const char * channel) {
+        backend->set_filter(boost::log::trivial::severity >= Level &&
+            boost::log::expressions::attr<std::string>("Channel") == channel);
+    }
+
+    // Generic function to map string severities to boost severities
+    inline void ChangeLevel(
+        backend_ptr & backend, const char * channel, const std::string & sev) {
+        if (backend) {
+            backend->reset_filter();
+            std::string lower(sev.size(), ' ');
+            std::transform(sev.cbegin(), sev.cend(), lower.begin(),
+                           [](char c) {return std::tolower(c, std::locale());});
+            if (lower == "debug") {
+                SetLevel<boost::log::trivial::debug>(backend, channel);
+            } else if (lower == "info") {
+                SetLevel<boost::log::trivial::info>(backend, channel);
+            } else if (lower == "warning") {
+                SetLevel<boost::log::trivial::warning>(backend, channel);
+            } else if (lower == "error") {
+                SetLevel<boost::log::trivial::error>(backend, channel);
+            } else if (lower == "off") {
+                SetLevel<boost::log::trivial::fatal>(backend, channel);
+            } else {
+                LOG(warning) << "Invalid log specifier: " << sev;
+                SetLevel<boost::log::trivial::trace>(backend, channel);
+            }
+        }
+    }
+
+    inline void ChangeGeneralLevel(const std::string& sev) {
+        ChangeLevel(general_backend(), "general", sev);
+    }
+    inline void ChangeTelemetryLevel(const std::string& sev) {
+        ChangeLevel(telemetry_backend(), "telemetry", sev);
+    }
+
+    inline void Initialize(const std::string& folder_name,
+            int general_size_mb, const std::string& general_file_name,
+            int telemetry_size_mb, const std::string& telemetry_file_name) {
+        std::string general_path = std::string("/home/logs/") + folder_name
+            + "/" + general_file_name + "_%N.log";
+        std::string target = std::string("/home/logs/") + folder_name;
+        std::string telemetry_path = std::string("/home/logs/") + folder_name
+            + "/" + telemetry_file_name + "_%N.log";
+        if (!general_backend()) {
+            general_backend() = boost::log::add_file_log(
+                boost::log::keywords::file_name = general_path.c_str(),
+                boost::log::keywords::open_mode = std::ios::out | std::ios::app,
+                // rotate files every 1MB
+                boost::log::keywords::rotation_size = 1024 * 1024,
+                // flush logfile to disk on write
+                boost::log::keywords::auto_flush = true,
+                boost::log::keywords::target = target.c_str(),
+                boost::log::keywords::max_size = general_size_mb * 1024*1024,
+                boost::log::keywords::format = "[%TimeStamp%]: %Message%");
+            ChangeGeneralLevel("info");
+        }
+        if (!telemetry_backend()) {
+            telemetry_backend() = boost::log::add_file_log(
+                boost::log::keywords::file_name = telemetry_path.c_str(),
+                boost::log::keywords::open_mode = std::ios::out | std::ios::app,
+                // rotate files every 1MB
+                boost::log::keywords::rotation_size = 1024 * 1024,
+                // flush logfile to disk on write
+                boost::log::keywords::auto_flush = true,
+                boost::log::keywords::target = target.c_str(),
+                boost::log::keywords::max_size = telemetry_size_mb * 1024*1024,
+                boost::log::keywords::format = "%TimeStamp%,%Message%");
+            ChangeTelemetryLevel("off");
+        }
+        boost::log::add_common_attributes();
+    }
+}
+
+#endif  // INCLUDE_MBCOREUTILS_FIRMWARE_LOGGING_H_
